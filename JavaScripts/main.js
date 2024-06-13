7c70433738cd991f28d89fee0ec6c59c5b573e4c
InovatorMind <tarnado445@gmail.com>
2024-06-13 23:22:52 +0530

    Fixed some Issue

--- JavaScripts/main.js
+++ JavaScripts/main.js
@@ -1,3 +1,4 @@
 function loadPage(page) {
   document.getElementById('contentFrame').src = page;
 }
+
--- JavaScripts/scroll-content.js
+++ JavaScripts/scroll-content.js
@@ -1,3 +1,29 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  const sections = document.querySelectorAll('section');
+
+  const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add('visible');
+              entry.target.classList.remove('hidden');
+          } else {
+              entry.target.classList.add('hidden');
+              entry.target.classList.remove('visible');
+          }
+      });
+  }, {
+      threshold: 0.1
+  });
+
+  sections.forEach(section => {
+      section.classList.add('hidden');
+      observer.observe(section);
+  });
+});
+
+
+
 document.getElementById('mobile-menu').addEventListener('click', function() {
   var sidebar = document.getElementById('sidebar');
   sidebar.classList.toggle('show');
@@ -17,3 +43,6 @@ function copyCode(button) {
       button.textContent = 'Copy';
   }, 2000);
 }
+
+
+
